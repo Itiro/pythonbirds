78a7eadf11573ea59fae7ab81bda99f30718c3d6
itiro <itiro_k@yahoo.com.br>
2021-01-07 15:11:15 -0300

    Criação método cumprimentar e atributos nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=47):
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Itiro')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Alexandre'
+    print(p.nome)
