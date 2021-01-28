de85dd7cee32185271bb065913fd5c1bf2f239cb
itiro <itiro_k@yahoo.com.br>
2021-01-28 07:34:48 -0300

    Atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=47):
+    def __init__(self, nome=None, idade=47, *filhos):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Itiro')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Alexandre'
-    print(p.nome)
+    alex = Pessoa(nome='Alex')
+    itiro = Pessoa('Itiro', 47, alex)
+    print(Pessoa.cumprimentar(itiro))
+    print(id(itiro))
+    print(itiro.cumprimentar())
+    print(itiro.nome)
+    for filho in itiro.filhos:
+        print(filho.nome)
